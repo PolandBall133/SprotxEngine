a4023e8c844d54928617d889eac97437067a77ef
Patryk Wertka <spartanpageagain@gmail.com>
2015-11-28 14:50:20 +0100

    Added aquiring data from settings and passing it forward for window_data filling

--- source/game_core/subsystems.cpp
+++ source/game_core/subsystems.cpp
@@ -9,14 +9,19 @@ using namespace std;
 
 void init(GameEngine &engine, const SettingsMap &settings){
     init_internal_subsystems(engine, settings);
+    init_window(engine, settings);
+    init_renderer(engine);
+}
+
+void finish(GameEngine &engine){
+    SDL_Quit();
 }
 
 tribool init_internal_subsystems(GameEngine &engine, const SettingsMap &settings){
-    using namespace settings::subsystems;
-    auto flags = settings.count(subsystems_flags_k)?
-        settings.at(subsystems_flags_k) == init_all_v?
+    auto flags = settings.count(settings::subsystems_flags)?
+        settings.at(settings::subsystems_flags) == settings::subsystems_init_all?
             INIT_EVERYTHING
-            : stoul(settings.at(subsystems_flags_k))
+            : stoul(settings.at(settings::subsystems_flags))
         : 0;
     BOOST_LOG_TRIVIAL(trace) << "Initializing game engine: calling #init_internal_subsystems (flags: " << flags << ")";
     return init_internal_subsystems(engine, flags);
@@ -36,6 +41,38 @@ tribool init_internal_subsystems(GameEngine &engine, uint32_t flags){
     return indeterminate;
 }
 
-void init_window_data(GameEngine &engine, const SettingsMap &settings){
+void init_window(GameEngine &engine, const SettingsMap &settings){
+    size_t width = stoul(settings.count(settings::window_width)?
+        settings.at(settings::window_width)
+        : settings::window_width_example);
 
+    size_t height = stoul(settings.count(settings::window_height)?
+        settings.at(settings::window_height)
+        : settings::window_height_example);
+
+    const char *title = settings.count(settings::window_title)?
+        settings.at(settings::window_title).c_str()
+        : settings::window_title_example;
+
+    BOOST_LOG_TRIVIAL(trace) << "Initializing game engine: calling #init_window"
+        << "(width: " << width << ", height: " << height << ")";
+    init_window(engine, width, height, title);
+}
+
+void init_window(GameEngine &engine, size_t width, size_t height, const char *title){
+    engine.window_data.window.reset(
+        SDL_CreateWindow(
+            title, 
+            SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 
+            width, height, 
+            SDL_WINDOW_SHOWN
+        )
+    );
+}
+
+void init_renderer(GameEngine &engine){
+    BOOST_LOG_TRIVIAL(trace) << "Initializing game engine: calling #init_renderer";
+    engine.window_data.renderer.reset(
+        SDL_CreateRenderer(engine.window_data.window(), -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC)
+    );
 }
