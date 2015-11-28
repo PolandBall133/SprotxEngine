2eebd208c49d735f506c83f7cccaf31df2c00565
Patryk Wertka <spartanpageagain@gmail.com>
2015-11-28 13:38:52 +0100

    Added SettingsMap type and macro for defining literals (related to settings map)

--- include/game_core/defines.hpp
+++ include/game_core/defines.hpp
@@ -1,4 +1,10 @@
 #pragma once
+#include <string>
+#include <map>
+
+using SettingsMap = std::map<std::string, std::string>;
 
 #define SDL_MAIN_HANDLED
 #define INIT_EVERYTHING SDL_INIT_EVERYTHING
+
+#define DEF_CONSTEXPR_STR_LITERAL(name) constexpr const char *name = #name
