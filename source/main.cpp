3ac570b42bb547382163efb6e04b1cc0bbb1a1e6
Patryk Wertka <spartanpageagain@gmail.com>
2015-11-28 12:42:23 +0100

    Now main runs the catch tests

--- source/main.cpp
+++ source/main.cpp
@@ -4,7 +4,18 @@
 #include "game_core/defines.hpp"
 #include "SDL.h"
 
+#define CATCH_CONFIG_RUNNER
+#include "catch.hpp"
+
+int run_tests(int argc, char **argv){
+    return Catch::Session().run(argc, argv);
+}
+
 int main(int argc, char **argv){
+    int tests_result = run_tests(argc, argv);
+    if(tests_result != 0)
+        return tests_result;
+
     GameEngine engine;
     init_subsystems(engine, INIT_EVERYTHING);
     return 0;
