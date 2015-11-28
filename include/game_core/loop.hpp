b5813ed991a571927472563eb6fb2f09d9d544fb
Patryk Wertka <spartanpageagain@gmail.com>
2015-11-28 16:07:54 +0100

    Loop, step by step (and some more)

--- include/game_core/loop.hpp
+++ include/game_core/loop.hpp
@@ -1,4 +1,23 @@
 #pragma once
 #include "game_engine.hpp"
 
-void loop(GameEngine &);
+template<typename Fpre, typename Fstep, typename Frender, typename Fpost>
+void loop(GameEngine &engine, Fpre &&pre_func, Fstep &&step_func, Frender &&render_func, Fpost &&post_func){
+    SDL_Event e;
+    bool running = true;
+    pre_func();
+    while(running){
+        while(SDL_PollEvent(&e)){
+            switch(e.type){
+            case SDL_QUIT:
+                running = false;
+                break;
+            }
+        }
+        step_func();
+        SDL_RenderClear(engine.window_data.renderer());
+        render_func();
+        SDL_RenderPresent(engine.window_data.renderer());
+    }
+    post_func();
+}
