9f0165fa03a11c6bd90e0319826739688d384589
Patryk Wertka <spartanpageagain@gmail.com>
2015-11-28 13:39:49 +0100

    Added few separated initial methods

--- include/game_core/subsystems.hpp
+++ include/game_core/subsystems.hpp
@@ -1,7 +1,13 @@
 #pragma once
 #include <boost/logic/tribool.hpp>
 
+#include "defines.hpp"
 #include "game_engine.hpp"
 
-void init(GameEngine &engine);
+void init(GameEngine &engine, const SettingsMap &settings);
+
+boost::tribool init_internal_subsystems(GameEngine &engine, const SettingsMap &settings);
+
 boost::tribool init_internal_subsystems(GameEngine &engine, uint32_t flags);
+
+void init_window_data(GameEngine &engine, const SettingsMap &settings);
