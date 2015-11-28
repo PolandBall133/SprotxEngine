9e4703471f42d84968675f8180637d042aaa0937
Patryk Wertka <spartanpageagain@gmail.com>
2015-11-28 13:07:30 +0100

    Sealed GameEngine class and added window_data (renderer and window resource handle)

--- include/game_core/game_engine.hpp
+++ include/game_core/game_engine.hpp
@@ -1,8 +1,19 @@
 #pragma once
 #include <cinttypes>
+#include <boost/utility.hpp>
 
-struct GameEngine{
+#include "defines.hpp"
+#include "SDL.h"
+ 
+#include "capi_helpers/rawcapiresource.hpp"
+
+struct GameEngine final{
     struct{
         uint32_t initialized_flags;
     } subsystems_info;
+    
+    struct{
+        RawCAPIResource<SDL_Renderer> renderer = {NULL};
+        RawCAPIResource<SDL_Window> window = {NULL};
+    } window_data;
 };
