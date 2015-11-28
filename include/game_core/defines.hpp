8e535b6369283b80c5ad91c1e30fbc01738fbecb
Patryk Wertka <spartanpageagain@gmail.com>
2015-11-28 14:48:14 +0100

    Added new macros fitting key-value settings approach

--- include/game_core/defines.hpp
+++ include/game_core/defines.hpp
@@ -1,10 +1,12 @@
 #pragma once
 #include <string>
 #include <map>
+#include <cctype>
 
 using SettingsMap = std::map<std::string, std::string>;
 
 #define SDL_MAIN_HANDLED
 #define INIT_EVERYTHING SDL_INIT_EVERYTHING
 
-#define DEF_CONSTEXPR_STR_LITERAL(name) constexpr const char *name = #name
+#define DEF_CONSTEXPR_STR_LITERAL_K(name) constexpr const char *name = #name
+#define DEF_CONSTEXPR_STR_LITERAL_V(name, val) constexpr const char *name = val
