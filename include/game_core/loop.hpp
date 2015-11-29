20c3ab424be5c1ca44cf62d732b8f67eb6a8cf36
Patryk Wertka <spartanpageagain@gmail.com>
2015-11-29 12:37:55 +0100

    There was too many templates

--- include/game_core/loop.hpp
+++ include/game_core/loop.hpp
@@ -1,23 +1,22 @@
 #pragma once
 #include "game_engine.hpp"
 
-template<typename Fpre, typename Fstep, typename Frender, typename Fpost>
-void loop(GameEngine &engine, Fpre &&pre_func, Fstep &&step_func, Frender &&render_func, Fpost &&post_func){
+template<typename Game>
+void loop(GameEngine &engine, Game &game){
     SDL_Event e;
-    bool running = true;
-    pre_func();
-    while(running){
+    game.pre();
+    while(game.is_running()){
         while(SDL_PollEvent(&e)){
             switch(e.type){
             case SDL_QUIT:
-                running = false;
+                game.set_running(false);
+                break;
+            case SDL_MOUSEMOTION:
                 break;
             }
         }
-        step_func();
-        SDL_RenderClear(engine.window_data.renderer());
-        render_func();
-        SDL_RenderPresent(engine.window_data.renderer());
+        game.step();
+        game.render();
     }
-    post_func();
+    game.post();
 }
