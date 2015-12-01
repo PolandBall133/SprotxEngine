b7e7e074d653141114c7521fdb945da301e75740
Patryk Wertka <spartanpageagain@gmail.com>
2015-12-01 15:33:57 +0100

    load_texture returns RawCAPIResource<SDL_Texture> instance (instead of raw SDL_Texture pointer); New render_text for better hello_worlds!

--- include/resources_managment/texture.hpp
+++ include/resources_managment/texture.hpp
@@ -1,8 +1,19 @@
 #pragma once
 #include <boost/filesystem.hpp>
 
+#include "capi_helpers/raw_capi_resource.hpp"
 #include "game_core/defines.hpp"
 #include "game_core/game_engine.hpp"
+#include "graphics_core/graphics_engine.hpp"
 #include "SDL.h"
+#include "SDL_ttf.h"
 
-SDL_Texture *load_texture(GameEngine &engine, const boost::filesystem::path &p);
+RawCAPIResource<SDL_Texture> load_texture(GameEngine &engine, const boost::filesystem::path &p);
+
+RawCAPIResource<SDL_Texture> render_text(
+    GraphicsEngine &graphics, 
+    TTF_Font *font, 
+    const std::string &text, 
+    SDL_Color color,
+    size_t size
+);
--- source/resources_managment/texture.cpp
+++ source/resources_managment/texture.cpp
@@ -1,7 +1,25 @@
 #include "resources_managment/texture.hpp"
 
 #include "SDL_image.h"
-SDL_Texture *load_texture(GameEngine &engine, const boost::filesystem::path &p){
-    return boost::filesystem::is_regular_file(p) ?
-        IMG_LoadTexture(engine.window_data.renderer(), p.string().c_str()) : nullptr;
+RawCAPIResource<SDL_Texture> load_texture(GameEngine &engine, const boost::filesystem::path &p){
+    auto internal = boost::filesystem::is_regular_file(p)?
+        IMG_LoadTexture(engine.window_data.renderer.get(), p.string().c_str()) : nullptr;
+
+    RawCAPIResource<SDL_Texture> result{
+        internal,
+        std::bind(SDL_DestroyTexture, std::placeholders::_1)
+    };
+
+    return result;
+}
+
+RawCAPIResource<SDL_Texture> render_text(
+    GraphicsEngine &graphics, TTF_Font *font, const std::string &text, SDL_Color color, size_t size)
+{
+    RawCAPIResource<SDL_Texture> result = {
+        graphics.render_text(font, text, color, size),
+        std::bind(SDL_DestroyTexture, std::placeholders::_1)
+    };
+
+    return result;
 }
