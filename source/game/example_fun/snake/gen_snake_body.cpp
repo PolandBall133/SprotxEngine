87b40929e9f45e3a7ae1843e2f8e32bab5e8e999
Patryk Wertka <spartanpageagain@gmail.com>
2015-12-02 18:30:47 +0100

    Lowered starting segments count

--- source/game/example_fun/snake/gen_snake_body.cpp
+++ source/game/example_fun/snake/gen_snake_body.cpp
@@ -1,7 +1,7 @@
 #include "game/example_fun/snake/gen_snake_body.hpp"
 
 Snake::Body gen_snake_body(){
-    const size_t segments_count = 40;
+    const size_t segments_count = 5;
     Snake::Body result;
     for(int i = 1; i <= segments_count; ++i)
         result.push_back({1, i});
