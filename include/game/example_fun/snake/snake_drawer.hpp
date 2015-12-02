ca343dda28808ad6b4b4ad556834be71b7d4e2c4
Patryk Wertka <spartanpageagain@gmail.com>
2015-12-02 18:30:59 +0100

    Made use out of aliases

--- include/game/example_fun/snake/snake_drawer.hpp
+++ include/game/example_fun/snake/snake_drawer.hpp
@@ -7,11 +7,8 @@
 struct SnakeDrawer{
     GraphicsEngine &graphics;
     const Snake &snake;
-    RawCAPIResource<SDL_Texture> &segment_tex;
+    TextureHandle &segment_tex;
 
-    SnakeDrawer(GraphicsEngine &g, const Snake &s, RawCAPIResource<SDL_Texture> &s_tex);
+    SnakeDrawer(GraphicsEngine &g, const Snake &s, TextureHandle &s_tex);
     void draw();
-
-    void initialize();
-    void finalize();
 };
