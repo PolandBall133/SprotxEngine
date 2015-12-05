c2fc0b3b0c83dfa1c085ea8a69983ccda9fa4190
Patryk Wertka <spartanpageagain@gmail.com>
2015-12-05 20:40:10 +0100

    The game isn't called directly anymore

--- include/game/example_fun/snake/game.hpp
+++ include/game/example_fun/snake/game.hpp
@@ -3,14 +3,13 @@
 #include "game_core/game_engine.hpp"
 #include "game_core/helpers/can_run.hpp"
 #include "game_core/helpers/mapping_keyboard_handler.hpp"
+#include "game_core/helpers/no_post.hpp"
 
 #include "graphics_core/graphics_engine.hpp"
 #include "graphics_core/text_drawer.hpp"
 
 #include "yse/yse.hpp"
 #include "fonts_core/fonts_system.hpp"
-
-#include "utility/directory_utils.hpp"
 
 #include "snake.hpp"
 #include "snake_drawer.hpp"
@@ -26,17 +25,15 @@ using boost::filesystem::path;
 using namespace Simplifications;
 
 namespace SnakeFun{
-    struct Game: CanRun{
+    struct Game: CanRun, NoPost{
         GameEngine &engine;
         GraphicsEngine &graphics;
+
+        FontsSystem &fonts_system;
         TextDrawer text_drawer;
 
-        const path resources_path;
+        const path &resources_path;
 
-        FontsSystem fonts_system;
-
-
-        TextureHandle background_tex;
         TextureHandle snake_segment_tex;
         TextureHandle fruit_tex;
 
@@ -54,13 +51,16 @@ namespace SnakeFun{
 
         SnakeInputHandler input_handler;
 
-        Game(GameEngine &e, GraphicsEngine &g);
+        Game(
+            GameEngine &e, 
+            GraphicsEngine &g, 
+            FontsSystem &fonts_system, 
+            const path &resources_path);
 
         MappingKeyboardHandler &get_keyboard_handler();
 
         void pre();
         void step();
         void render();
-        void post();
     };
 }
--- source/game/example_fun/snake/game.cpp
+++ source/game/example_fun/snake/game.cpp
@@ -5,16 +5,14 @@ using namespace boost::chrono;
 
 namespace SnakeFun{
     const Limiter game_limiter = {-1, -1, 64, 48};
-    Game::Game(GameEngine &e, GraphicsEngine &g):
+    Game::Game(GameEngine &e, GraphicsEngine &g, FontsSystem &fs, const path &rp):
         engine(e), 
         graphics(g),
         text_drawer(graphics),
+        fonts_system(fs),
 
-        resources_path(greedy_locate_directory("resources") + "example_fun/snake/"),
+        resources_path(rp),
 
-        fonts_system(graphics, resources_path),
-
-        background_tex(load_texture(engine, resources_path + "background.png")),
         snake_segment_tex(load_texture(engine, resources_path + "segment.png")),
         fruit_tex(load_texture(engine, resources_path + "fruit.png")),
 
@@ -34,8 +32,6 @@ namespace SnakeFun{
     }
 
     void Game::pre(){
-        YSE::System().init();
-
         eating_sound.create(
             (resources_path + "suck-up.wav").string().c_str(),
             &YSE::ChannelFX()
@@ -54,26 +50,15 @@ namespace SnakeFun{
         static size_t counter = 0;
         if(++counter >= 3){
             snake.move();
-
             collider.check_and_react();
-
             counter = 0;
-
-            YSE::System().update();
         }
     }
 
     void Game::render(){
-        graphics.begin_render({170, 220, 255});
-        graphics.draw(background_tex.get(), 0, 0);
         auto text = "snake score: " + to_string(score_manager.score());
         text_drawer.draw(fonts_system.font("sample_font").of_size(64), text, 10, 10);
         fruits_drawer.draw();
         snake_drawer.draw();
-        graphics.end_render();
-    }
-
-    void Game::post(){
-        YSE::System().close();
     }
 }
