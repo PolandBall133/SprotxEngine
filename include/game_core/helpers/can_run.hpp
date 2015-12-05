0ac0b8c97fb0b50e0f9fc788ab28e890bcf24457
Patryk Wertka <spartanpageagain@gmail.com>
2015-12-05 20:37:27 +0100

    Whops, const-correctness

--- include/game_core/helpers/can_run.hpp
+++ include/game_core/helpers/can_run.hpp
@@ -3,7 +3,7 @@
 namespace Simplifications{
     struct CanRun{
         bool running = true;
-        bool is_running()
+        bool is_running() const
         { return running; }
         void set_running(bool value)
         { running = value; }
