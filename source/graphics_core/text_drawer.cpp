65cd07ba93f9d394b1f692ff666cd951ef06ec87
Patryk Wertka <spartanpageagain@gmail.com>
2015-12-05 20:37:50 +0100

    Bleh, ColorMod didn't worked

--- source/graphics_core/text_drawer.cpp
+++ source/graphics_core/text_drawer.cpp
@@ -13,15 +13,13 @@ void TextDrawer::draw(
     SDL_Color color)
 {
     for(size_t i = 0; i < text.size(); ++i){
-        auto tex = font.glyph(text[i]).texture.get();
-
-        SDL_SetTextureColorMod(tex, color.r, color.g, color.b);
+        auto &tex = font.glyph(text[i], color).texture;
 
         int minx, maxx, miny, maxy, advance;
         if(TTF_GlyphMetrics(font.raw_font(), 'g', &minx, &maxx, &miny, &maxy, &advance) == -1)
             BOOST_LOG_TRIVIAL(error) << TTF_GetError();
 
         x += (advance + space);
-        graphics.draw(tex, x, y);
+        graphics.draw(tex.get(), x, y);
     }
 }
